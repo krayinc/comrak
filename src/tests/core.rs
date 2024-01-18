e0f62739bcf1638a1daf7d4cc5d6cee8c8e416be
massan <y.m.arittake@gmail.com>
2024-01-18 16:50:30 +0900

    feat: 画像URLの後ろに複数の半角スペースを許容する

--- src/tests/core.rs
+++ src/tests/core.rs
@@ -300,6 +300,13 @@ fn image_size() {
             "<p><img src=\"https://image.example.com/test.png%20=WxH\" alt=\"test.png\" /></p>\n"
         ),
     );
+    // URLの後にスペースが複数ある場合
+    html(
+        concat!("![test.png](https://image.example.com/test.png   =WxH)\n",),
+        concat!(
+            "<p><img src=\"https://image.example.com/test.png%20%20%20=WxH\" alt=\"test.png\" /></p>\n"
+        ),
+    );
 }
 
 #[test]
